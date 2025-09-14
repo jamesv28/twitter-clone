5c624498fdca94cefa1e81bc0c37db79fa66bdea
James Volmert <james.volmert@gmail.com>
2025-09-13 18:47:31 -0600

    added basic ui layout for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import HomePage from "./pages/home/HomePage";
-
+import ProfilePage from "./pages/profile/ProfilePage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import Sidebar from "./components/common/Sidebar";
 import "./App.css";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
     </div>
   );
