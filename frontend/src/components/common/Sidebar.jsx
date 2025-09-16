1d5dc5594ed84b9f1a3672878fe6847a9c9b7aa5
James Volmert <james.volmert@gmail.com>
2025-09-15 18:00:04 -0600

    logout functionality

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -1,12 +1,35 @@
 import XSvg from "../svgs/X";
-
+import { useMutation } from "@tanstack/react-query";
 import { MdHomeFilled } from "react-icons/md";
 import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
+import toast from "react-hot-toast";
 
 const Sidebar = () => {
+  const { mutate: logoutMutation, error } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch("/api/auth/logout", {
+          method: "POST",
+        });
+
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.error || "Something went wrong");
+      } catch (error) {
+        toast.error(`Error: ${error.message}`);
+        throw new Error(error.message);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Logout Successful");
+    },
+    onError: () => {
+      toast.error(`Error occurred: ${error.message}`);
+    },
+  });
+
   const data = {
     fullName: "John Doe",
     username: "johndoe",
@@ -66,7 +89,13 @@ const Sidebar = () => {
                 </p>
                 <p className="text-slate-500 text-sm">@{data?.username}</p>
               </div>
-              <BiLogOut className="w-5 h-5 cursor-pointer" />
+              <BiLogOut
+                className="w-5 h-5 cursor-pointer"
+                onClick={(e) => {
+                  e.preventDefault();
+                  logoutMutation();
+                }}
+              />
             </div>
           </Link>
         )}
