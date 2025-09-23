14e53e17df11c0211028d890fc36778e103693f8
James Volmert <james.volmert@gmail.com>
2025-09-23 17:54:50 -0600

    edit posts in profile

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -16,7 +16,7 @@ const router = express.Router();
 router.get("/all", protectRoute, getAllPosts);
 router.get("/following", protectRoute, getFollowingPosts);
 router.get("/user/:username", protectRoute, getUserPosts);
-router.get("/like/:id", protectRoute, getLikedPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
